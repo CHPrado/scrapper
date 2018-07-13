3941667f5fa388b917134f8faa5ce09c9fc2cf11
CHPrado <christianprado.contato@gmail.com>
2018-07-13 19:15:32 -0300

    Gerador de planilha Excel

--- conexaoDB.py
+++ conexaoDB.py
@@ -1,71 +1,102 @@
 import sqlite3
+from xlwt import Workbook
 
 class ConexaoSQL:
   def getConexao(self):
     return sqlite3.connect('scrapper.db')
 
-  def inserirImovel(self, descricao, area, precoVenda, precoAluguel, pais, 
+  def inserirImovel(self, id, descricao, area, precoVenda, precoAluguel, pais, 
               estado, cidade, regiao, bairro, logradouro, numero):
     conn = self.getConexao()
     c = conn.cursor()
-    c.execute("INSERT INTO imovel (descricao, area, precoVenda, precoAluguel, pais, estado, cidade, regiao, bairro, logradouro, numero) VALUES(?,?,?,?,?,?,?,?,?,?,?)",
-              (descricao, area, precoVenda, precoAluguel, pais, estado, cidade, regiao, bairro, logradouro, numero))
-    print("Imóvel adicionado...")
+    c.execute("INSERT INTO imovel (id, descricao, area, precoVenda, precoAluguel, pais, estado, cidade, regiao, bairro, logradouro, numero) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)",
+              (id, descricao, area, precoVenda, precoAluguel, pais, estado, cidade, regiao, bairro, logradouro, numero))
     conn.commit()
 
   def filtrarBairro(self, bairro):
     conn = self.getConexao()
     c = conn.cursor()
-    sql = "SELECT * FROM imovel WHERE bairro = '" + bairro.upper() + "'"
+    sql = "SELECT * FROM imovel WHERE bairro LIKE '%" + bairro.upper() + "%' ORDER BY precoVenda"
     c.execute(sql)
     dados_imoveis = c.fetchall()
 
     if len(dados_imoveis) > 0:
-      for imovel in dados_imoveis:
-        output = "Área: {}m². ".format(imovel[2])
-        if imovel[3] == 'None':
-          output = output + "Valor de Aluguel: R$ {0:2.2f}.".format(imovel[4])
-        else:
-          output = output + "Valor de Venda: R$ {0:2.2f}.".format(imovel[3])
-
-        print(output)
+      nome_arquivo = 'Bairro {}'.format(bairro.upper())
+      self.gerarExcel(nome_arquivo, dados_imoveis)
     else:
       print("Nenhum registro encontrado.")
 
   def filtrarMetragem(self, metragem):
     conn = self.getConexao()
     c = conn.cursor()
-    sql = "SELECT * FROM imovel WHERE area = '" + metragem + "'"
+    sql = "SELECT * FROM imovel WHERE area = '" + metragem + "' ORDER BY precoVenda"
     c.execute(sql)
     dados_imoveis = c.fetchall()
 
     if len(dados_imoveis) > 0:
-      for imovel in dados_imoveis:
-        output = "Área: {}m². ".format(imovel[2])
-        if imovel[3] == 'None':
-          output = output + "Valor de Aluguel: R$ {0:2.2f}.".format(imovel[4])
-        else:
-          output = output + "Valor de Venda: R$ {0:2.2f}.".format(imovel[3])
-
-        print(output)
+      nome_arquivo = 'Metragem {}'.format(metragem.upper())
+      self.gerarExcel(nome_arquivo, dados_imoveis)
     else:
       print("Nenhum registro encontrado.")
 
   def filtrarPredio(self, logradouro, numero):
     conn = self.getConexao()
     c = conn.cursor()
-    sql = "SELECT * FROM imovel WHERE logradouro = '" + logradouro + "' AND numero = '" + numero + "'"
+    sql = "SELECT * FROM imovel WHERE logradouro LIKE '%" + logradouro + "%' AND numero = '" + numero + "' ORDER BY precoVenda"
     c.execute(sql)
     dados_imoveis = c.fetchall()
 
     if len(dados_imoveis) > 0:
-      for imovel in dados_imoveis:
-        output = "Área: {}m². ".format(imovel[2])
-        if imovel[3] == 'None':
-          output = output + "Valor de Aluguel: R$ {0:2.2f}.".format(imovel[4])
-        else:
-          output = output + "Valor de Venda: R$ {0:2.2f}.".format(imovel[3])
-
-        print(output)
+      nome_arquivo = 'Predio logradouro - {}'.format(logradouro.upper())
+      self.gerarExcel(nome_arquivo, dados_imoveis)
     else:
       print("Nenhum registro encontrado.")
+  
+  def listarTodos(self):
+    conn = self.getConexao()
+    c = conn.cursor()
+    sql = "SELECT * FROM imovel ORDER BY precoVenda"
+    c.execute(sql)
+    dados_imoveis = c.fetchall()
+
+    if len(dados_imoveis) > 0:
+      nome_arquivo = 'LISTA COMPLETA SCRAPPER'
+      self.gerarExcel(nome_arquivo, dados_imoveis)
+    else:
+      print("Nenhum registro encontrado.")
+
+  def gerarExcel(self, nome_arquivo, dados_imoveis):
+    wb = Workbook()
+    sheet1 = wb.add_sheet('Página 1', cell_overwrite_ok=True)
+    sheet1.col(0).width = 15000
+    sheet1.col(1).width = 5000
+    sheet1.col(2).width = 5000
+    sheet1.col(3).width = 5000
+    sheet1.col(4).width = 10000
+    sheet1.col(5).width = 5000
+    sheet1.col(6).width = 5000
+
+    sheet1.write(0, 0, 'Descrição')
+    sheet1.write(0, 1, 'Área (m²)')
+    sheet1.write(0, 2, 'Valor de Venda R$')
+    sheet1.write(0, 3, 'Valor de Aluguel R$')
+    sheet1.write(0, 4, 'Logradouro')
+    sheet1.write(0, 5, 'Número')
+    sheet1.write(0, 6, 'Bairro')
+
+    i = 1
+    for imovel in dados_imoveis:
+      sheet1.write(i, 0, imovel[1])
+      sheet1.write(i, 1, imovel[2])
+      sheet1.write(i, 2, imovel[3])
+      sheet1.write(i, 3, imovel[4])
+      sheet1.write(i, 4, imovel[10])
+      sheet1.write(i, 5, imovel[11])
+      sheet1.write(i, 6, imovel[9])
+
+      i += 1
+    try:
+      wb.save('{}.xls'.format(nome_arquivo))
+      print('Planilha Excel "{}" gerada'.format(nome_arquivo))
+    except:
+      print('Erro ao gerar planilha Excel. Talvez um arquivo com mesmo nome esteja aberto. Feche o arquivo e tente novamente.')
--- scrapper.py
+++ scrapper.py
@@ -6,27 +6,46 @@ from conexaoDB import ConexaoSQL
 
 class ZapImoveis:
   def __init__(self):
-    self.SCRAPPING = 100 #QUANTIDADE DE IMOVEIS A SEREM VARRIDOS POR EXECUÇÃO
-    self.url = 'https://www.zapimoveis.com.br/tr/imoveis/sp'
+    self.QTD_IMOVEIS = 30 #QUANTIDADE MÍNIMA DE IMÓVEIS A SEREM SCANNEADOS DO SITE
+    self.pagina = 1 #PAGINA ATUAL
+    #URL INICIAL
+    self.url = 'https://www.zapimoveis.com.br/tr/imoveis/sp+sao-paulo/?gclid=CjwKCAjw1ZbaBRBUEiwA4VQCIUwVQNUVCZnEDpCYPLQ4dKPqhIQ4IkOsxiudPNjn7XmXEeoKQhi5HxoCbZUQAvD_BwE#{"precomaximo":"2147483647","parametrosautosuggest":[{"Bairro":"","Zona":"","Cidade":"SAO%20PAULO","Agrupamento":"","Estado":"SP"}],"pagina":"1","ordem":"Relevancia","paginaOrigem":"ResultadoBusca","semente":"1413743582","formato":"Lista"}'
     self.agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
     self.conn = ConexaoSQL()
 
   def _get_descricao(self, bs_detalhe):
-    return bs_detalhe.find('div', class_='descricao').text.replace('\n', '').replace('\r', '').replace('Ver descrição completa', '')
+    try:
+      descricao = bs_detalhe.find('div', class_='descricao').text.replace('\n', '').replace('\r', '').replace('Ver descrição completa', '')
+    except:
+      descricao = bs_detalhe.find('span', class_='descricao').text
+    return descricao
 
   def _get_area(self, bs_detalhe):
-    return str(bs_detalhe.find('input', {'id' : 'areaUtil'}).get('value'))
+    try:
+      area = str(bs_detalhe.find('input', {'id' : 'areaUtil'}).get('value'))
+    except:
+      area = str(bs_detalhe.find('input', {'id' : 'hdnareaMin'}).get('value'))
+    return area
 
   def _get_precoVenda(self, bs_detalhe):
-    return str(bs_detalhe.find('input', {'id' : 'precoVenda'}).get('value'))
+    try:
+      preco = str(bs_detalhe.find('input', {'id' : 'precoVenda'}).get('value'))
+    except:
+      preco = bs_detalhe.find('span', class_='dados-ficha').text
+    return preco
   
   def _get_precoAluguel(self, bs_detalhe):
     return str(bs_detalhe.find('input', {'id' : 'precoAluguel'}).get('value'))
 
   def _get_endereco(self, bs_detalhe):
-    return re.split('[;]', str(bs_detalhe.find('input', {'id' : 'address'}).get('value')))
+    try:
+      endereco = re.split('[;]', str(bs_detalhe.find('input', {'id' : 'address'}).get('value')))
+    except:
+      endereco = re.split('[;]', str(bs_detalhe.find('input', {'id' : 'hdnsaddress'}).get('value')))
+    return endereco
 
   def listarImoveis(self):
+    self.url = 'https://www.zapimoveis.com.br/tr/imoveis/sp+sao-paulo/?gclid=CjwKCAjw1ZbaBRBUEiwA4VQCIUwVQNUVCZnEDpCYPLQ4dKPqhIQ4IkOsxiudPNjn7XmXEeoKQhi5HxoCbZUQAvD_BwE#{"precomaximo":"2147483647","parametrosautosuggest":[{"Bairro":"","Zona":"","Cidade":"SAO%20PAULO","Agrupamento":"","Estado":"SP"}],"pagina":"'+str(self.pagina)+'","ordem":"Relevancia","paginaOrigem":"ResultadoBusca","semente":"1413743582","formato":"Lista"}'
     main_page = requests.get(self.url, headers=self.agent)
     bs = BS(main_page.text, 'lxml')
     return bs.find_all('article', class_='minificha')
@@ -35,65 +54,6 @@ class ZapImoveis:
     url_imovel = imovel.find('a').get('href')
     detalhe_imovel = requests.get(url_imovel, headers=self.agent)
     return BS(detalhe_imovel.text, 'lxml')
-
-  def navegar(self):
-    print("Varrendo imóveis de Zap Imoveis...")
-    #NAVEGA NA PÁGINA PRINCIPAL E COLETA AS URL DOS IMÓVEIS LISTADOS
-    lista_imoveis = self.listarImoveis()
-    qtd_imoveis = 0
-    for imovel in lista_imoveis:
-      bs_detalhe = self.detalhesImovel(imovel)
-
-      #IMÓVEIS COM STATUS 'EM OBRA' TEM A PÁGINA DIFERENTE DOS DEMAIS, POR ISSO
-      #AS PÁGINAS CONTEM TAGS DIFERENTES FAZENDO COM QUE RETORNE NONETYPE
-      #OBS: A MAIORIA COM PREÇOS SOB CONSULTA.
-      try: 
-        descricao = self._get_descricao(bs_detalhe)
-        area = self._get_area(bs_detalhe)
-        endereco = self._get_endereco(bs_detalhe)
-        precoVenda = self._get_precoVenda(bs_detalhe)
-        precoAluguel = self._get_precoAluguel(bs_detalhe)
-
-        pais = endereco[0].upper()
-        estado = endereco[1].upper()
-        cidade = endereco[2].upper()
-        regiao = endereco[3].upper()
-        bairro = endereco[4].upper()
-        logradouro = endereco[5].upper()
-        numero = endereco[6]
-        # id = int(endereco[7])
-
-        # print('ID: {}'.format(id))
-        # print('Descrição: {}\n'.format(descricao))
-        # print('Área : {}m², Preço Venda: {}, Preço Aluguel: {}'.format(area, precoVenda, precoAluguel))   
-        # print('Logradouro: {}, nº: {}, Bairro: {}, Regiao: {}, Cidade: {}, Estado: {}\n\n'.format(logradouro, numero, bairro, regiao, cidade, estado))
-
-        self.conn.inserirImovel(descricao, area, precoVenda, precoAluguel, pais, 
-                                estado, cidade, regiao, bairro, logradouro, numero)
-        qtd_imoveis += 1
-
-      except: #PARA PÁGINAS COM STATUS 'EM OBRA'. 
-        pass
-        # descricao = bs_detalhe.find('span', class_='descricao').text
-        # areaMin = str(bs_detalhe.find('input', {'id' : 'hdnareaMin'}).get('value'))
-        # areaMax = str(bs_detalhe.find('input', {'id' : 'hdnareaMax'}).get('value'))
-        # preco = bs_detalhe.find('span', class_='dados-ficha').text
-        # endereco = re.split('[;]', str(bs_detalhe.find('input', {'id' : 'hdnsaddress'}).get('value')))
-        
-        # pais = endereco[0]
-        # estado = endereco[1]
-        # cidade = endereco[2]
-        # regiao = endereco[3]
-        # bairro = endereco[4]
-        # logradouro = endereco[5]
-        # numero = endereco[6]
-        # id = endereco[7]
-
-        # print('Descrição: {}\n'.format(descricao))
-        # print('Área mínima: {}m², Área máxima: {}, Preço: {}'.format(areaMin, areaMax, preco))   
-        # print('Logradouro: {}, nº: {}, Bairro: {}, Regiao: {}, Cidade: {}, Estado: {}\n\n'.format(logradouro, numero, bairro, regiao, cidade, estado))
-
-    print("Scrapper finalizado. {} imóveis adicionados no banco de dados.".format(qtd_imoveis))
 
   def filtrarBairro(self):
     bairro = input("Informe o bairro: ")
@@ -107,6 +67,56 @@ class ZapImoveis:
     logradouro = input("Informe o logradouro: ")
     numero = input("Informe o número do prédio: ")
     self.conn.filtrarPredio(logradouro, numero)
+  
+  def listarTodos(self):
+    self.conn.listarTodos()
+
+  def navegar(self):
+    print("Varrendo imóveis de Zap Imoveis...")
+    qtd_imoveis = 0
+    #NAVEGA NA PÁGINA PRINCIPAL E COLETA AS URL DOS IMÓVEIS LISTADOS
+    while True:
+      print("Página {}...".format(self.pagina))
+      lista_imoveis = self.listarImoveis()
+      
+      for imovel in lista_imoveis:
+        bs_detalhe = self.detalhesImovel(imovel)
+
+        #IMÓVEIS COM STATUS 'EM OBRA' TEM A PÁGINA DIFERENTE DOS DEMAIS, POR ISSO
+        #AS PÁGINAS CONTEM TAGS DIFERENTES FAZENDO COM QUE RETORNE NONETYPE
+        #OBS: A MAIORIA COM PREÇOS SOB CONSULTA.
+        try: 
+          descricao = self._get_descricao(bs_detalhe)
+          area = self._get_area(bs_detalhe)
+          endereco = self._get_endereco(bs_detalhe)
+          precoVenda = self._get_precoVenda(bs_detalhe)
+          precoAluguel = self._get_precoAluguel(bs_detalhe)
+
+          pais = endereco[0].upper()
+          estado = endereco[1].upper()
+          cidade = endereco[2].upper()
+          regiao = endereco[3].upper()
+          bairro = endereco[4].upper()
+          logradouro = endereco[5].upper()
+          numero = endereco[6]
+          id = int(endereco[7])
+
+          self.conn.inserirImovel(id, descricao, area, precoVenda, precoAluguel, pais, 
+                                  estado, cidade, regiao, bairro, logradouro, numero)
+          qtd_imoveis += 1
+
+        except: # Exception as e:  
+          # print(e)
+          pass
+
+      #MANTEM O LOOP ENQUANTO NAO ATINGIR A QUANTIDADE MÍNIMA DE IMÓVEIS 
+      if qtd_imoveis >= self.QTD_IMOVEIS:
+        break
+      else:
+        #PASSA PARA PRÓXIMA PÁGINA
+        self.pagina += 1
+        
+    print("Scrapper finalizado. {} imóveis adicionados no banco de dados.".format(qtd_imoveis))
 
   def iniciar(self):
     while True:
@@ -115,6 +125,7 @@ class ZapImoveis:
         "2. Filtrar por Metragem.\n"
         "3. Filtrar por Prédio.\n"
         "4. Filtrar por Bairro.\n"
+        "5. Gerar planilha com todos os imóveis.\n"
         "0. Sair.\n")
 
       option = input("Opção: ")
@@ -127,6 +138,8 @@ class ZapImoveis:
         zap.filtrarPredio()
       elif option == '4':
         zap.filtrarBairro()
+      elif option =='5':
+        zap.listarTodos()
       elif option == '0':
         break
 
